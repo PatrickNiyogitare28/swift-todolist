b60f4f16b3c19643b48f7643b1e67f1f09295154
Patrick Niyo <patrickniyogitare28@gmail.com>
2025-01-02 12:28:43 +0200

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Patrick Niyo on 02/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
